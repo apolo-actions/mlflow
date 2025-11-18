b44251fb8a179b64b2e28821fae8e571354c4bb6
Jordy Antunes <jordy.antunes@hotmail.com>
2025-11-18 10:19:05 -0300

    Added outputs server

---
+++ outputs-server/main.py
@@ -0,0 +1,50 @@
+from contextlib import asynccontextmanager
+import logging
+import os
+from typing import Any, Dict, List
+import requests
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+
+server_variables = {}
+logger = logging.getLogger(__name__)
+
+class BasicResponse(BaseModel):
+    status: str
+    data: Dict[str, Any] | List[Dict[str, Any]] | None = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        MAIN_APP_DEPLOYMENT_NAME = os.environ["MAIN_APP_DEPLOYMENT_NAME"]
+        MAIN_APP_DEPLOYMENT_NAMESPACE = os.environ["MAIN_APP_DEPLOYMENT_NAMESPACE"]
+        MLFLOW_PORT = os.environ["MLFLOW_PORT"]
+        MLFLOW_URL = f"http://{MAIN_APP_DEPLOYMENT_NAME}.{MAIN_APP_DEPLOYMENT_NAMESPACE}.svc.cluster.local:{MLFLOW_PORT}"
+        server_variables['MLFLOW_URL'] = MLFLOW_URL
+        logger.info(f"MLFLOW_URL set to {MLFLOW_URL}")
+    except KeyError as e:
+        raise RuntimeError(f"Environment variable {e} is not set") from e
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get('/')
+@app.get('/health')
+@app.get('/healthz')
+async def root() -> BasicResponse:
+    return BasicResponse(status="healthy")
+
+@app.get('/output')
+async def get_outputs(filter: str | None = None):
+    try:
+        res = requests.get(server_variables['MLFLOW_URL'] + '/api/2.0/mlflow/registered-models/search', params={'filter': filter})
+        res.raise_for_status()
+        json_response = res.json()
+        return BasicResponse(status="success", data=json_response.get("registered_models", []))
+    except requests.RequestException as e:
+        return {'error': str(e)}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
